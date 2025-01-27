7d8a88c6688b6274bb69e3e9a50c77b46df9b3b0
Renan <renan.g.rodrigues5@gmail.com>
2025-01-26 21:00:34 -0300

    26/01

--- Orientação de objetos/banco.py
+++ Orientação de objetos/banco.py
@@ -1,85 +1,48 @@
 class Banco():
-    def __init__(self):
-        self.pessoasCadastradas = 0
-        self.Clientes = {
-        }
+    def __init__(self,nomeBanco):
+        self.nomeBanco = nomeBanco
+        self.Clientes = []
      
 
-    def CadastrarClientes(self, nome, rg, endereco, email):
-        self.chave = (nome, rg, endereco, email)
-        if self.chave not in self.Clientes:
-            self.Clientes[self.chave] = True #adiconando os valoers na chave
-            self.pessoasCadastradas += 1
-            return F'{self. Clientes}'
-        else:
-            print("Deu errado")
-    
-    def exibirClientes (self):
-       while True:
-        for self.chave in self.Clientes:
-            return f"Nome: {self.chave[0]}, RG: {self.chave[1]}, Endereço: {self.chave[2]}, Email: {self.chave[3]}"
+    def CadastrarClientes(self, id, nome, saldoCliente):
+        self.id = id
         
+        self.Clientes.append(
+            {'id' : id,
+            'nome' : nome,
+            'Saldo do Cliente' : saldoCliente
+            })
 
-    def abrirConta (self,nome, rg, endereco, email,tipoConta, valor = 100):
-        self.valor = valor
-        self.chave = (nome,rg,endereco,email)
+    def abrirConta (self, alternativa, valor,id_cliente, transferencia_de_Conta = None):
+        operacao = self.Clientes[id_cliente]
 
-        if self.chave in self.Clientes:
-            numero_conta = f"Banco: {rg}, {nome}, {self.pessoasCadastradas}"
-
-            conta = {
-                "Tipo de conta" : tipoConta,
-                "saldo" : valor
-            }
-
-            self.Clientes[numero_conta] = conta
-
-            print(f"cliente do numero da conta {numero_conta}")
-        else:
-            print(f"{nome} não cadastrado")
-
-    def operacoes(self):
-        print("1 para sacar")
-        print("2 para depositar")
-        print("3 para transferencia")
-        
-        operacao = int(input("Digite um número: "))
-        if operacao == 1:
-            saque = int(input("Digite quanto você quer sacar: "))
-            if saque < self.valor:
-                operacaoSaque = self.valor - saque
-                return f"você sacou,sua conta estava com {self.valor} e agora está com {operacaoSaque} e seu saque de {saque}, foi sucedida"
-            else:
-                print("erro")
-
-        elif operacao == 2:
-            depositando = int(input("Digite quanto você quer depositar: "))
-            OperacaoDeposito = depositando + self.valor
-            return f'Seu deposito ocorreu com sucesso: valor antigo {self.valor} e agora é {OperacaoDeposito},depois do seu depósito de {depositando} '
-        
-
-        elif operacao == 3:
-            pessoa = input("Digite o nome da pessoa que você deseja fazer uma transferencia: ")
-            if pessoa in self.Clientes:
-                print("Conta verificada")
-                transferencia = int(input("Digite o valor da transferência: "))
-                if transferencia < self.valor:
-                    return "transferência realizada"
-                else:
-                    return "deu errado"
+                
+        if alternativa == 1:
+            operacao['Saldo do Cliente'] -= valor
+            print(f'o {operacao["nome"]} agora tem: {operacao["Saldo do Cliente"]}')
+     
+        elif alternativa == 2:
+            operacao['Saldo do Cliente'] += valor
+            print(f'o {operacao["nome"]} agora tem: {operacao["Saldo do Cliente"]}')
+       
+        elif alternativa == 3 and id is not None:
+            operacao['Saldo do Cliente'] -= valor
+            transferencia_de_Conta = self.Clientes[transferencia_de_Conta]
+            transferencia_de_Conta['Saldo do Cliente'] += valor
+            print(f'o {operacao["nome"]} transferiu {valor} para: {transferencia_de_Conta["nome"]}')
                     
 
 
+cliente = Banco("Bradesco")
+usuario = Banco("Inter")
 
-test = Banco()
 
+cliente.CadastrarClientes(0,"renan",50)
+cliente.CadastrarClientes(1,"gabriel",80)
 
-test.CadastrarClientes("renan", 87293, "rua brasilia de pelotas", "manoqueraiva@gmail.com" )
-test.CadastrarClientes("marian", 58393, "rua laurisca de franca", "finamente@gmail.com" )
+usuario.CadastrarClientes(0,"victor",80)
+usuario.CadastrarClientes(1,"kaka",65)
 
-print(test.abrirConta(nome="marian", rg=58393, endereco="rua laurisca de franca", email="finamente@gmail.com",tipoConta="Conta Corrente"))
-print(test.abrirConta(nome="renan", rg=87293, endereco="rua brasilia de pelotas", email="manoqueraiva@gmail.com",tipoConta="Conta Corrente"))
-
-print(test.operacoes())
-print(test.exibirClientes())
-
+cliente.abrirConta(1,10,0)
+cliente.abrirConta(2, 20, 1) 
+cliente.abrirConta(3, 30, 0, 1)
--- Orientação de objetos/lojaVirtual.py
+++ Orientação de objetos/lojaVirtual.py
@@ -1,17 +1,36 @@
 class lojaVirtual():
-    def __init__(self):
-        self.produtos = {}
-        pass
+    def __init__(self,nomeLoja):
+        self.nomeLoja = nomeLoja
+        self.produtos = []
+       
     
-    def cadastrarProduto ():
-        while True:
-            inserindoProdutos = input("Deseja cadastrar algum produto: ")
-            if inserindoProdutos == "não":
-                print("produto não cadastrado")
-                break
+    def cadastrarProduto (self, nomeProduto,preco):
+
+            codigoDoProduto = len(self.produtos)
             
-            nomeProdutos = input("Digite o nome do produto: ")
-            codigoDoProduto = int(input("Digite o codigo do produto: "))
+            self.dadosProduto ={
+                "codigo" : codigoDoProduto,
+                "Produto" : nomeProduto,
+                "preço" : preco,
+                
+            }     
+            
+            self.produtos.append(self.dadosProduto) 
+            
+    def carrinho (self, escolhaProduto, quantidade, desconto=0):
+        self.dadosCarrinho = self.dadosProduto
+        self.valorTotal = self.produtos[escolhaProduto]['preço'] * quantidade - desconto
         
+        self.dadosCarrinho.pop('codigo')
+        self.dadosCarrinho.update({'quantidade': quantidade, 'Valor Total': self.valorTotal})
         
+        return self.dadosCarrinho
     
+    
+    
+loja = lojaVirtual('paracabauP')
+
+loja.cadastrarProduto('tenis', 20)
+loja.cadastrarProduto('chuteiora', 10)
+
+print(loja.carrinho(1,10,1))
