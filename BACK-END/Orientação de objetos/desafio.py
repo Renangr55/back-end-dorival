3d2557244d8cb82a4d6744c24ebb51ae723bb5ba
Renangr55 <campinas.ets@br.bosch.com>
2025-03-05 11:28:35 -0300

    finalizando ultimo exercício de POO

--- BACK-END/Orientação de objetos/desafio.py
+++ BACK-END/Orientação de objetos/desafio.py
@@ -1,87 +1,84 @@
-class Evento():
-    def __init__(self,nomeEvento:str, local:str, descricao:str):
+class Evento:
+    def __init__(self, nomeEvento: str, local: str, descricao: str):
         self.nomeEvento = nomeEvento
         self.local = local
         self.descricao = descricao
         
     def __str__(self):
-        return f" nome do evento: {self.nomeEvento} | local: {self.local} | descrição: {self.descricao}"
-    
+        return f"nome do evento: {self.nomeEvento} | local: {self.local} | descrição: {self.descricao}"
 
-        
-        
+
 class Tarefa(Evento):
-    def __init__(self, nomeEvento, local, descricao,TarefaDesejada,responsavel,prazo,status):
+    def __init__(self, nomeEvento, local, descricao, TarefaDesejada, responsavel, prazo, status):
         super().__init__(nomeEvento, local, descricao)
-        
         self.tarefasDesejada = TarefaDesejada
         self.responsavel = responsavel
         self.prazo = prazo
         self.status = status
         
-    def aluguel(self,local):
-        pagamento_aluguel = 2000
-        
-    def comida(self,comdaDesejada):
-        pagamento_comida = 500
-        
     def __str__(self):
         return f"nome: {self.nomeEvento} | Local: {self.local} | tarefa desejada: {self.tarefasDesejada} | responsavel: {self.responsavel} | prazo: {self.prazo} | status: {self.status}"
 
+
 class Fornecedor(Evento):
-    def __init__(self, nomeEvento, local, descricao, nomeFornecedor,servicoFornecido):
+    def __init__(self, nomeEvento, local, descricao, nomeFornecedor, servicoFornecido):
         super().__init__(nomeEvento, local, descricao)
-        
         self.nomeFornecedor = nomeFornecedor
         self.servicoFornecido = servicoFornecido
-        
         
     def __str__(self):
         return f"fornecedor: {self.nomeFornecedor} | serviço fornecido: {self.servicoFornecido}"
     
-class responsavel(Evento):
-    def __init__(self, nomeEvento, local, descricao,responsavelEvento):
+
+class Responsavel(Evento):
+    def __init__(self, nomeEvento, local, descricao, responsavelEvento):
         super().__init__(nomeEvento, local, descricao)
-        
         self.responsavelEvento = responsavelEvento
     
     def __str__(self):
-        return f"Evento: {self.nomeEvento} | responsavel pelo evento: {self.responsavelEvento}"
-    
+        return f"Evento: {self.nomeEvento} | responsável pelo evento: {self.responsavelEvento}"
+
+
 class Convidado(Evento):
-    def __init__(self, nomeEvento, local, descricao, quantidadePessoas):
+    def __init__(self, nomeEvento, local, descricao, quantidadePessoas: int):
         super().__init__(nomeEvento, local, descricao)
-    
         self.quantidadePessoas = quantidadePessoas
-    def __str__(self):
-        return f"Evento: {self.nomeEvento} | quantidade de pessoas: {self.quantidadePessoas}"
     
+    def __str__(self):
+        return f"Evento: {self.nomeEvento} | quantidade de pessoas: {self.quantidadePessoas} convidados"
+
+
 class Pagamento(Tarefa):
-    def __init__(self, nomeEvento, local, TarefaDesejada):
-        super().__init__(nomeEvento, local, TarefaDesejada)
+    def __init__(self, nomeEvento, local, descricao, TarefaDesejada, responsavel, prazo, status, pagamentoTarefa):
+        super().__init__(nomeEvento, local, descricao, TarefaDesejada, responsavel, prazo, status)
+        self.pagamentoTarefa = pagamentoTarefa
         
-    def realizarPagamento(self,valordisponivel,valorAluguel,valorTarefa):
-        
-        if valordisponivel > valorAluguel:
-            print("Pagamento realizado do local")
+    def realizarPagamento(self, valordisponivel: int, valorTarefa: int):
+        if valordisponivel >= valorTarefa:
+            print("Pagamento realizado da tarefa")
         else:
-            print("Pagemnto não realizado")
-            
-        if valordisponivel > valorTarefa:
-            print(f"Pagmento realzado da {valorTarefa}")
-        else:
-            print("pagamento não foi realizado")
+            print("Pagamento não realizado")
             
     def __str__(self):
-        return f"nomeEvento: {self.nomeEvento} | realizando pagamento"
-    
-    
-festa = Evento("Alyson's party", "Germany", "festa do meu querido alyson")
+        return f"nome de Evento: {self.nomeEvento} | realizando pagamento: {self.pagamentoTarefa}" 
+
+
+festa = Evento("Alyson's party", "Germany", "festa do meu querido Alyson")
 print(festa)
 
-festa = Tarefa
-        
-    
-        
-            
-        
+festa = Tarefa("Alyson's party", "Germany", "festa do meu querido Alyson", "alugar um espaço", "Renan", "em andamento", "22/01/2025")
+print(festa)
+
+festa = Fornecedor("Alyson's party", "Germany", "festa do meu querido Alyson", "Seara", "churrasco")
+print(festa)
+
+festa = Responsavel("Alyson's party", "Germany", "festa do meu querido Alyson", "Lucca Dias")
+print(festa)
+
+festa = Convidado("Alyson's party", "Germany", "festa do meu querido Alyson", 100)
+print(festa)
+
+festa = Pagamento("Alyson's party", "Germany", "festa do meu querido Alyson", "alugar um espaço", "Renan", "em andamento", "22/01/2025", "pagamento da alimentação")
+print(festa)
+
+festa.realizarPagamento(5000, 400)
