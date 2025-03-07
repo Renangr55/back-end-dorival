9ca247e37f3960734455d55eeb179b4d2c507908
Renan Gabriel Rodrigues Mendon�a <49278351890@>
2025-03-07 16:55:13 -0300

    CRUD-ALUNO finalizado

---
+++ BACK-END/Django/API-REST/crud_estudante/estudante/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('alunos/', views.listar_alunos),
+    path('alunos/criar/', views.criar_aluno)
+
+]
---
+++ BACK-END/Django/API-REST/crud_estudante/estudante/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Aluno
+from .serializers import AlunoSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+
+# Create your views here.
+@api_view(['GET'])
+def listar_alunos(request):
+    alunos = Aluno.objects.all()
+    serializer = AlunoSerializer(alunos, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def criar_aluno(request):
+    if request.method == 'POST':
+        serializer = AlunoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
