47394fee9cb84f7f49d42e158fafeee5e3f49253
Renan Gabriel Rodrigues Mendon�a <49278351890@>
2025-03-13 17:00:07 -0300

    Api de estudantes - FINALIZADA

--- BACK-END/Django/API-REST/crud_estudante/estudante/urls.py
+++ BACK-END/Django/API-REST/crud_estudante/estudante/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('alunos/', views.listar_alunos),
-    path('alunos/criar/', views.criar_aluno)
+    path('alunos/criar/', views.criar_aluno),
+    path('alunos/<int:pk>/', views.detalhe_aluno),
+    path('alunos/alterar/<int:pk>/', views.alterar_aluno),
+    path('alunos/apagar/<int:pk>/', views.deletar_aluno),
+    path('faz_algo/<str:texto>' , views.macharete),
 
 ]
--- BACK-END/Django/API-REST/crud_estudante/estudante/views.py
+++ BACK-END/Django/API-REST/crud_estudante/estudante/views.py
@@ -6,12 +6,24 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 
+
 # Create your views here.
 @api_view(['GET'])
 def listar_alunos(request):
     alunos = Aluno.objects.all()
     serializer = AlunoSerializer(alunos, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def detalhe_aluno(request,pk):
+    try: 
+        aluno = Aluno.objects.get(pk=pk)
+    except Aluno.DoesNotExist:
+        return Response({'erro': 'não foi encontrado esse aluno'},status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = AlunoSerializer(aluno)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 @api_view(['POST'])
 def criar_aluno(request):
@@ -21,4 +33,38 @@ def criar_aluno(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
+@api_view(['PUT'])
+def alterar_aluno(request,pk):
+    try:
+        aluno = Aluno.objects.get(pk=pk)
+
+    except Aluno.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = AlunoSerializer(aluno, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def deletar_aluno(request,pk):
+    try:
+        aluno = Aluno.objects.get(pk=pk)
+    except Aluno.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    aluno.delete()
+    return Response({'aluno excluido'},status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["GET"])
+def macharete(request, texto):
+    import pyfiglet
+    gabriel = pyfiglet.figlet_format(texto)
+    
+    return Response(gabriel, status=status.HTTP_200_OK)
+   
