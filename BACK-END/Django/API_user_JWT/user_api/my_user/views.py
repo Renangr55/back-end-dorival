7a8a47e10027fe4e32a1a9ddcb88945f56cf11d9
Renan <renan.g.rodrigues5@gmail.com>
2025-03-30 18:39:47 -0300

    30/03 - jwt_finalizado

--- BACK-END/Django/API_user_JWT/user_api/my_user/views.py
+++ BACK-END/Django/API_user_JWT/user_api/my_user/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
-from .models import User, UserAdmSite
+from .models import UserAdmSite , AbstractUser
 from .serializers import userSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 
-
 # Create your views here.
 
 @api_view(['GET','POST'])
-def createListUser(request):
+@permission_classes([IsAuthenticated])
+def create_list_user(request):
 
-    user = UserAdmSite.objects.all()
 
     if request.method == 'GET':
-       serializer = userSerializer(user, many=True)
-       return Response(serializer.data, status=status.HTTP_200_OK)
+        user = UserAdmSite.objects.all()
+        serializer = userSerializer(user, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
        
     
@@ -28,8 +28,10 @@ def createListUser(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+   
 @api_view(['GET','PUT', 'DELETE'])
-def updateDelete(request,pk):
+@permission_classes([IsAuthenticated])
+def update_delete(request,pk):
 
 
     if request.method == 'GET':
@@ -54,14 +56,17 @@ def updateDelete(request,pk):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
         
-        if request.method == 'DELETE':
-            try:
-                user = UserAdmSite.objects.get(pk=pk)
-            except UserAdmSite.DoesNotExist:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            
-            user.delete()
-            return Response({f"User removed: {user}"}, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        try:
+            user = UserAdmSite.objects.get(pk=pk)
+        except UserAdmSite.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         
-                
+        user.delete()
+        return Response({f"User removed: {user}"}, status=status.HTTP_200_OK)
+    
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protect_view (request):
+        return Response({"mensagem": "Olá, você está autenticado com JWT!"}, status=status.HTTP_200_OK)
