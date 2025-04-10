ede5d967c064ed38c4c3b679cf08c2156250fb53
Renan Gabriel Rodrigues Mendon�a <49278351890@>
2025-04-10 15:28:57 -0300

    10/04

---
+++ BACK-END/Django/ModelView/ds14/patos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Patithos (models.Model):
+    nome = models.CharField(max_length=20)
+    especie = models.CharField(max_length=150)
+    idade = models.PositiveIntegerField()
+    peso = models.FloatField()
+    cor = models.CharField(max_length=50)
+    superPoder = models.CharField(max_length=200)
+    cagaTorrada = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        if self.cagaTorrada:
+            return f'{self.nome} caga torradas perfeitas'
+        return f'{self.nome} caga torradas perfeitas'
